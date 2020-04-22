a3c0779c3e5e3bfb62c543dccb58d5a131465971
Benjamin Lofo Follo <benjamin.lofo@gmail.com>
2020-04-22 17:48:04 -0400

    fix tslint issues

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import bodyParser from "body-parser";
+import express from "express";
+import {deleteLocalFiles, filterImageFromURL} from "./util/util";
 
 (async () => {
 
@@ -9,40 +9,39 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
-  
+
   // Displays a simple message to the user
   app.get( "/filteredimage", async (req, res) => {
-    const imageUrl: string|null = req.query.image_url
+    const imageUrl: string|null = req.query.image_url;
 
     // Check if the image URL is existing
     if (!imageUrl) {
-        return res.status(400).send({ message: 'image URL is required' });
+        return res.status(400).send({ message: "image URL is required" });
     }
 
     try {
-        let filePath = await filterImageFromURL(req.query.image_url);
+        const filePath = await filterImageFromURL(req.query.image_url);
         res.sendFile(filePath, (error) => {
             if (error) {
-                res.status(500).send('Error while transfering file');
+                res.status(500).send("Error while transfering file");
             }
-            deleteLocalFiles([filePath])
+            deleteLocalFiles([filePath]);
         });
     } catch (e) {
         // Catch the error if one occurs
-        res.status(400).send({ message: 'An error has occured. Please verify your image URL'})
+        res.status(400).send({ message: "An error has occured. Please verify your image URL"});
     }
-    
+
   });
 
   // Root Endpoint
   // Displays a simple message to the user
   app.get("/", async (req, res) => {
-    res.send("try GET /filteredimage?image_url={{}}")
+    res.send("try GET /filteredimage?image_url={{}}");
   } );
-  
 
   // Start the Server
   app.listen( port, () => {
